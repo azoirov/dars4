c52275b597072f09cf3ac7f0f63548e434f68214
Asadbek <zoirovasadbek2002@gmail.com>
2021-08-10 22:07:55 +0500

    dars1

---
+++ app.js
@@ -0,0 +1,103 @@
+// Boolean
+// Switch
+// loop
+// DOM
+
+// let name = prompt("Ismingizni kiriting");
+
+// if(name){
+// 	console.log(name)
+// } else {
+// 	console.log("Xayr")
+// }
+
+// concatenation
+
+let arr = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+
+let newArr = [...arr, ...arr2]
+// rest spread
+
+// Switch
+let color = "blue";
+
+// switch (color) {
+
+// 	case "red": {
+// 		console.log("qizil")
+// 		break
+// 	}
+
+// 	case "black": {
+// 		console.log("qora")
+// 		break
+// 	}
+
+// 	default: {
+// 		console.log("topolmadim")
+// 	}
+
+// }
+
+// RegExp
+// let comment = prompt("leave comment please");
+
+// let badWords = new RegExp("yomon", "gi")
+// console.log(comment.replace(badWords, "***"))
+
+// match(Regexp)
+// replace("nimani almashtirish", "nimaga almashtirish")
+// split(nima bo'yicha)
+
+// let withoutBad = comment.split(badWords)
+// join
+
+// console.log(withoutBad.join("***"))
+// Berilgan matnda
+// - 4 ta gap
+// - 15 ta so'z
+// - 50 ta belgi
+
+
+// let text = prompt("Matn kiriting");
+
+// let words = text.split(" ").length;
+// let char = text.split("").length;
+// let howManySentence = new RegExp("[.?!]", "g");
+// console.log(howManySentence)
+// let sectences = text.split(howManySentence).length - 1
+
+// alert(`Bu matnda:\n- ${sectences} ta gap\n- ${words} ta so'z\n- ${char} ta belgi bor`)
+
+// for forEach
+// let num = 100;
+// let i = 1
+// while (i>0) {
+// 	console.log(i)
+// 	i++
+// }
+
+// do {
+// 	console.log(i);
+// 	i++
+// } while (i<=100)
+
+//
+
+// for(let i = 0; i<100; i++) {
+	// array[i]
+// }
+
+// for of for in
+// for(let element of ["olma", "anor"]) {
+	// let i = array.indexOf(element)
+// 	console.log(element)
+// }
+// let obj = {x: 2, y: 5}
+// for (let i in obj) {
+// 	console.log(obj[i])
+// }
+
+// npm
